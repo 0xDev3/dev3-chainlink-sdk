bf8137f0fe04eeb1085723a81d904f03557ce435
evmbro <filip@ampnet.io>
2023-09-27 22:30:10 +0200

    update package

---
+++ dist/node/dev3-sdk.mjs
@@ -0,0 +1,7 @@
+import { ethers } from 'ethers';
+
+var u=(s,n,e)=>new Promise((t,d)=>{var a=r=>{try{o(e.next(r));}catch(i){d(i);}},g=r=>{try{o(e.throw(r));}catch(i){d(i);}},o=r=>r.done?t(r.value):Promise.resolve(r.value).then(a,g);o((e=e.apply(s,n)).next());});var p=class{constructor(n,e){this._aggregatorV3Interface=["function decimals() external view returns (uint8)","function description() external view returns (string memory)","function version() external view returns (uint256)","function getRoundData(uint80 _roundId) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)","function latestRoundData() external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)"];this._provider=new ethers.providers.JsonRpcProvider(n,e.network),this.feeds=e;}getFromOracle(n){return u(this,null,function*(){var a;let t=yield new ethers.Contract(n.address,this._aggregatorV3Interface,this._provider).latestRoundData();return {roundID:t[0],answer:t[1],startedAt:t[2],updatedAt:t[3],answeredInRound:t[4],dataFeedName:(a=n.name)!=null?a:""}})}};
+
+export { p as Dev3ChainlinkSDK };
+//# sourceMappingURL=out.js.map
+//# sourceMappingURL=dev3-sdk.mjs.map
---
+++ tsup.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'tsup';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import plugin from 'node-stdlib-browser/helpers/esbuild/plugin';
+import stdLibBrowser from 'node-stdlib-browser';
+
+export default defineConfig([
+  {
+    name: 'browser',
+    entry: ['src/dev3-sdk.ts'],
+    sourcemap: true,
+    platform: 'browser',
+    clean: true,
+    minify: true,
+    shims: true,
+    treeshake: true,
+    globalName: 'Dev3ChainlinkSDK',
+    format: ['cjs', 'esm'],
+    noExternal: ['cbor'],
+    inject: ['node_modules/node-stdlib-browser/helpers/esbuild/shim.js'],
+    esbuildPlugins: [NodeModulesPolyfillPlugin(), plugin(stdLibBrowser)],
+    outDir: 'dist/browser',
+  },
+  {
+    name: 'node',
+    entry: ['src/dev3-sdk.ts'],
+    sourcemap: true,
+    platform: 'node',
+    clean: true,
+    minify: true,
+    shims: true,
+    treeshake: true,
+    globalName: 'Dev3ChainlinkSDK',
+    format: ['cjs', 'esm'],
+    outDir: 'dist/node',
+  },
+]);
